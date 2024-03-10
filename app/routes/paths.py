49698888a0af6961ad96aa82b51ecef622647d58
AndryAlexis <andry.html@gmail.com>
2024-03-10 20:18:33 +0100

    Ahora con ReactPy
    Empezado

--- app/routes/paths.py
+++
@@ -1,2 +0,0 @@
-HOME = '/'
-MULTIPLE_CHOICE_TEST = '/multiple-choice-test'
--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-from app.routes import routes
-import flet as ft
+from reactpy import component, html
+from reactpy.backend.fastapi import configure
+from fastapi import FastAPI
 
-from app.ai.trainedai import DriversExamination
+@component
+def main():
+    return html.h1('Hello world! xd')
 
-KEY = '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'
-question = 'Cuando está intentando entrar a la autopista, debe conducir:'
-short_answer = 'A la misma o casi la misma velocidad que el tráfico en la autopista.'
-long_answer = 'Cuando se incorpore a una autopista, usted debe entrar a la velocidad del tránsito, o cerca de ella.'
-
-trained_ai = DriversExamination(api_key=KEY)
-response = trained_ai.send_message(question=question,
-                                   short_answer=short_answer,
-                                   long_answer=long_answer)
-
-if (response is None):
-    print('ALGO HA PASAO')
-    exit()
-
-print(response)
-# print(response['question'], end='\n\n')
-# print('\t',response['answers'][0]['text'], response['answers'][0]['isCorrect'])
-# print('\t',response['answers'][1]['text'], response['answers'][1]['isCorrect'])
-# print('\t',response['answers'][2]['text'], response['answers'][2]['isCorrect'])
-# print('\t',response['answers'][3]['text'], response['answers'][3]['isCorrect'])
-# print('\n')
-
-# if __name__ == '__main__':
-#     ft.app(target=routes, view=ft.AppView.WEB_BROWSER)
+app = FastAPI()
+configure(app, main)
